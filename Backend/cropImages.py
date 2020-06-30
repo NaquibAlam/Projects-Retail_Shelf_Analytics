4564fdde257ad7180fc55457f7d091f144b906dc
NaquibAlam <anaquib165@gmail.com>
2020-07-01 03:51:33 +0530

    Update cropImages.py

--- Backend/cropImages.py
+++ Backend/cropImages.py
@@ -3,11 +3,11 @@ import cv
 import numpy as np
 from PIL import Image
 
-image_path = '/data1/paritosh.pandey/CIGDataset/ShelfImages'
-annotations_path = '/data1/paritosh.pandey/CIGDataset/BBOX'
+image_path = '/data1/naquib.alam/CIGDataset/ShelfImages'
+annotations_path = '/data1/naquib.alam/CIGDataset/BBOX'
 for filename in os.listdir(path):
    img = Image.open(path + '/' + filename)
    ing = np.array(img)
    ing = ing[:, :, ::-1].copy()
    
-
+   
